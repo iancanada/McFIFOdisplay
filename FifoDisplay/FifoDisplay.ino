f08d2d3aa35d8cda3397df51c7cdd8794026229d
iancanada <iancanada.mail@gmail.com>
2017-06-13 20:52:09 -0400

    Update2017-06-10

--- FifoDisplay/FifoDisplay.ino
+++ FifoDisplay/FifoDisplay.ino
@@ -1,3 +1,29 @@
+/**
+  **************************************************************************************
+  * File Name          : FifoDisplay
+  * Date of create     : 2017-06-10
+  * Author             : Ian Jin  
+  *                    : iancanada.mail@gmail.com
+  *                    : Twitter: @iancanadaTT
+  *                    
+  * Description        : Open source Arduino based McFIFO display project
+  * 
+  * This program is a free software: you can redistribute it and/or modify it
+  * under the terms of the GNU Lesser General Public License as published by the
+  * Free Software Foundation, either version 3 of the License, or any later
+  * version.
+  *
+  * This program is distributed in the hope that it will be useful, but WITHOUT
+  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+  * for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License
+  * along with this program. If not, see <http://www.gnu.org/licenses/>.        
+  * **************************************************************************************
+  * COPYRIGHT(c) Ian Jin All rights reserved
+  */
+
 #include <LiquidCrystal.h>
 #include "FifoMessage.hpp"
 
@@ -7,8 +33,8 @@ package_t package;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);           // start serial port
-  lcd.begin(16, 2);              // start16*2 LCD
+  Serial.begin(9600);               // start serial port
+  lcd.begin(16, 2);                 // start16*2 LCD
   lcd.setCursor(0,0);lcd.print("Ian I2S/DSD FIFO");
   lcd.setCursor(0,1);lcd.print("    DISPLAY     ");
 }
--- FifoDisplay/FifoMessage.hpp
+++ FifoDisplay/FifoMessage.hpp
@@ -1,3 +1,29 @@
+/**
+  **************************************************************************************
+  * File Name          : FifoMessage.cpp
+  * Date of create     : 2017-06-10
+  * Author             : Ian Jin  
+  *                    : iancanada.mail@gmail.com
+  *                    : Twitter: @iancanadaTT
+  *                    
+  * Description        : Open source Arduino based McFIFO display project
+  * 
+  * This program is a free software: you can redistribute it and/or modify it
+  * under the terms of the GNU Lesser General Public License as published by the
+  * Free Software Foundation, either version 3 of the License, or any later
+  * version.
+  *
+  * This program is distributed in the hope that it will be useful, but WITHOUT
+  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+  * for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public License
+  * along with this program. If not, see <http://www.gnu.org/licenses/>.        
+  * **************************************************************************************
+  * COPYRIGHT(c) Ian Jin All rights reserved
+  */
+
 #ifndef FIFOMESSAGE_H_
 #define FIFOMESSAGE_H_
 #include <arduino.h>
